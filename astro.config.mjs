b5bd4ea1ef650a3f80cd18dd94091e706735fd5b
jairoelano <jairo.evalle@gmail.com>
2025-04-05 10:48:44 -0300

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
-
-
-
-
 // https://astro.build/config
 export default defineConfig({
   output:'static',
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,6 @@ module.exports = {
 	},
 	plugins: [
 		require('flowbite/plugin'),
-		require('tailwind-scrollbar'),
+		
 	],
 }
